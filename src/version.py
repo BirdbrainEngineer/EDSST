3ccd67da71c8985298965629d463e7f85766ae30
Birdbrain <Birdbrain.engineer@gmail.com>
2026-02-08 23:15:08 +0100

    testing mode from config.toml file

--- src/version.py
+++ src/version.py
@@ -1,12 +1,15 @@
 from pathlib import Path
 from enum import Enum, auto
+import toml
 
-EDSST_VERSION = "v0.1.1"
+EDSST_VERSION = "v0.1.2"
 
 MODULE_VERSIONS_PATH = Path("module_versions.json")
+
+config = toml.load("config.toml")
 
 class TestingMode(Enum):
     Testing = auto()
     Release = auto()
 
-TESTING_MODE: TestingMode = TestingMode.Testing
+TESTING_MODE: TestingMode = TestingMode.Testing if config.get("testing_mode", False) else TestingMode.Release
